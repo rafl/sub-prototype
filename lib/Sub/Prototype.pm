72346df3966a67f5c0e9ac444ebefda8a38699ef
Florian Ragwitz <rafl@debian.org>
2008-11-09 06:36:42 +0100

    Add documentation.

--- lib/Sub/Prototype.pm
+++ lib/Sub/Prototype.pm
@@ -15,3 +15,47 @@ use Sub::Exporter -setup => {
 };
 
 1;
+
+__END__
+
+=head1 NAME
+
+Sub::Prototype - Set a subs prototype
+
+=head1 SYNOPSIS
+
+    use Sub::Prototype;
+
+    BEGIN {
+        my $code = sub { ... };
+        set_prototype($code, '&@');
+        *main::my_func = $code;
+    }
+
+    my_func { ... } @list;
+
+=head1 FUNCTIONS
+
+=head2 set_prototype(\&coderef, $prototype)
+
+    set_prototype(\&some_function, '$$');
+
+Sets the prototype for C<coderef> to C<$prototype>.
+
+=head1 THANKS
+
+Shawn M Moore for the idea and tests.
+
+=head1 AUTHOR
+
+Florian Ragwitz E<lt>rafl@debian.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2008  Florian Ragwitz
+
+This module is free software.
+
+It may distribute it under the same terms as perl itself.
+
+=cut
